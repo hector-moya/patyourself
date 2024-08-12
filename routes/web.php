4ef71ffa05941c6bc06df2a733b9898c51a0f244
Hector <nokure@gmail.com>
2024-08-12 19:56:04 +1000

    starting plans page

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Models\Plan;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlanController;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function ()
+{
+    Route::get('/plans',        [PlanController::class, 'show'])->name('plans.show');
+});
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () 
+{
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
